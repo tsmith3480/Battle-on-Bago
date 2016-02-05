e00ccff3c0d2470a3fa156199adf9ce3f7b4de1d
Tony <smith7214@fvtc.edu>
2016-02-04 19:14:30 -0600

    Medoo/composer add

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
-    phpinfo();
+    require 'vendor/autoload.php';
+	
+	$database = new medoo([
+		'database_type' => 'mssql',
+		'database_name' => 'BOB',
+		'server' => 'agilebob.database.windows.net',
+		'username' => 'bobadmin',
+		'password'=> 'Agilebob60515',
+		'charset' => 'utf8'
+	]);
+	
+	
 ?>
